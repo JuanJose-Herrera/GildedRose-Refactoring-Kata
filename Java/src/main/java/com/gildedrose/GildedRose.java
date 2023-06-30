857674cb52e8930776b8d23eeb4785e27980a0d9
Juan Jose Herrera <juan@netx.net>
2023-06-29 19:24:42 -0600

    Add conjured unit test and business logic

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,87 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+
+            //update Quality for Aged Brie and Backstage
+            if (item.name.equals("Aged Brie")
+                || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+                //item can never have its Quality increase above 50
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    //"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        //Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                        //Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
                     }
                 }
+
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                if (item.name.startsWith("Conjured")) {
+                    item.quality = item.quality - 2;
+                    //The Quality of an item is never negative
+                    if(item.quality<0){
+                        item.quality = 0;
                     }
+                } else
+                //normal product quality degradation
+                //Sulfuras never degrade
+                if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            //Sulfuras never has to be sold all other proudcts get the sellIn properties updated
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            //Once the sell by date has passed, Quality degrades twice as fast
+            if (item.sellIn < 0) {
+                //"Aged Brie" actually increases in Quality the older it gets
+                if (item.name.equals("Aged Brie")) {
+                    //item can never have its Quality increase above 50
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+
+                           // "Conjured" items degrade in Quality twice as fast as normal items
+                            if (item.name.startsWith("Conjured")) {
+                                item.quality = item.quality - 2;
+                                //The Quality of an item is never negative
+                                if(item.quality<0){
+                                    item.quality = 0;
+                                }
+                            } else
+                                //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                // normal product Once the sell by date has passed, Quality degrades twice as fast
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        item.quality = 0;
                     }
                 }
             }
-        }
+        }//end for
+
     }
+
+
 }
